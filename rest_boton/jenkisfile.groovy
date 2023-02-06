5221b974208eb802c5ae5c2ba0aa801ce5bbb8ca
GrupoE5 <122994833+GrupoE5@users.noreply.github.com>
2023-02-06 08:43:52 -0500

    Update jenkisfile.groovy

--- rest_boton/jenkisfile.groovy
+++ rest_boton/jenkisfile.groovy
@@ -7,6 +7,7 @@ pipeline {
         stage('Build') {
             steps {
                 sh 'java -version'
+                sh './gradlew build'
                 sh 'java -jar ./build/libs/rest-1.0.jar'
             }
         }
